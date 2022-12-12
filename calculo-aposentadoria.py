afd1515ad1140e9f6f86e33c963f6eb0cf3dfd56
bshlarissa <bslarissa1112@gmail.com>
2022-12-12 17:15:30 -0300

    first commit

---
+++ calculo-aposentadoria.py
@@ -0,0 +1,4 @@
+nome = input("Qual seu nome?")
+idade = int(input("Quantos anos vc tem?"))
+ano=str((2022-idade)+65)
+print (nome, "voce vai aposentar em",ano)
---
+++ sorvete.py
@@ -0,0 +1,4 @@
+nome = input("Qual seu nome?")
+print (nome, "vc gosta de sorvete?")
+resposta = input("Responda sim ou nao")
+print (nome,resposta,"de sorvete")
